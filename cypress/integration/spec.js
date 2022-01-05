0d33963d6ad0f77b792fa462074fd199106e63b1
Rohith <rohith.singh@appsierra.com>
2022-01-05 10:31:04 +0530

    second commit

---
+++ cypress/integration/spec.js
@@ -0,0 +1,12 @@
+describe('Myfirstprogram', function(){
+
+    it('My first test case', function(){
+    
+    cy.visit("https://www.rahulshettyacademy.com/AutomationPractice/");
+    
+    
+    
+    })
+    
+    
+    })
